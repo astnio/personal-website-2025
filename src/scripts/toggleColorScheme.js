03d845d33a3a5b5e219b56b19ee9fbece065e992
astio <github@mail.astn.io>
2024-11-18 09:03:48 -0800

    Color scheme toggler now works

--- src/scripts/toggleColorScheme.js
+++ src/scripts/toggleColorScheme.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //todo
+  const documentRoot = document.querySelector(':root');
+  const lightToggle = document.getElementById('btn-light-toggle');
+
+  function getSavedColorScheme() {
+    return localStorage.getItem('colorScheme');
+  }
+
+  function saveColorScheme(colorScheme) {
+    localStorage.setItem('colorScheme', colorScheme);
+  }
+
+  function setColorScheme(colorScheme) {
+    documentRoot.setAttribute('data-color-scheme', colorScheme);
+  }
+
+  function toggleColorScheme() {
+    console.log('click');
+    if (getSavedColorScheme() === 'dark') {
+      setColorScheme('light');
+      saveColorScheme('light');
+    } else if (getSavedColorScheme() === 'light') {
+      setColorScheme('dark');
+      saveColorScheme('dark');
+    }
+  }
+
+  lightToggle.addEventListener('click', toggleColorScheme);
 });
