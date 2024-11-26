424e9170ad4220746415c90293a6aab9888a8327
astio <github@mail.astn.io>
2024-11-25 18:18:45 -0800

    Changed appbar movement to be based on a transform instead of its top position

--- src/scripts/appBar.js
+++ src/scripts/appBar.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.getElementById('app-bar');
+
   let prevScrollPos = window.scrollY;
-  const navbarHeight = navbar.scrollHeight;
 
   window.onscroll = function () {
     let currentScrollPos = window.scrollY;
     if (prevScrollPos > currentScrollPos) {
-      navbar.style.top = '0';
+      navbar.style.transform = 'translateY(0%)';
     } else {
-      navbar.style.top = String(-navbarHeight + 'px');
+      navbar.style.transform = 'translateY(-100%)';
     }
     prevScrollPos = currentScrollPos;
   };
