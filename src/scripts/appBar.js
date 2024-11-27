246a3a75b5f695beb2e6e0b755a4f6ddb8c7f155
astio <github@mail.astn.io>
2024-11-27 00:35:31 -0800

    Fixed issue of appbar not moving when user scrolls

--- src/scripts/appBar.js
+++ src/scripts/appBar.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('astro:page-load', () => {
   const navbar = document.getElementById('app-bar');
 
   const scrollActivationDistance = 150;
