c11dad58949dad40f0d6743d2f1d322bb8ae28db
astio <github@mail.astn.io>
2024-11-26 22:52:59 -0800

    Added delay to appbar appearing/disappearing when scrolling

--- src/scripts/appBar.js
+++ src/scripts/appBar.js
@@ -3,13 +3,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let prevScrollPos = window.scrollY;
 
+  const scrollActivationHeight = 150;
+
+  let totalScrollDistance = 0;
+  let userIsScrollingDown = true;
+  let userWasScrollingDown = false;
+
+  function hideAppBar() {
+    navbar.style.transform = 'translateY(-100%)';
+  }
+
+  function revealAppBar() {
+    navbar.style.transform = 'translateY(0%)';
+  }
+
+  function updateScrollDireciton(currScrollDir) {
+    if (currScrollDir > 0) {
+      userIsScrollingDown = true;
+    } else if (currScrollDir < 0) {
+      userIsScrollingDown = false;
+    }
+
+    if (userIsScrollingDown !== userWasScrollingDown) {
+      totalScrollDistance = 0;
+    }
+
+    userWasScrollingDown = userIsScrollingDown;
+  }
+
   function handleAppBarOnScroll() {
     let currentScrollPos = window.scrollY;
-    if (prevScrollPos > currentScrollPos) {
-      navbar.style.transform = 'translateY(0%)';
-    } else {
-      navbar.style.transform = 'translateY(-100%)';
+
+    let currentScrollDirection = currentScrollPos - prevScrollPos;
+    let currentScrollDistance = Math.abs(currentScrollPos - prevScrollPos);
+
+    totalScrollDistance = totalScrollDistance + currentScrollDistance;
+
+    updateScrollDireciton(currentScrollDirection);
+
+    if (!userIsScrollingDown && totalScrollDistance > scrollActivationHeight) {
+      revealAppBar();
+      totalScrollDistance = 0;
+    } else if (
+      userIsScrollingDown &&
+      totalScrollDistance > scrollActivationHeight
+    ) {
+      hideAppBar();
+      totalScrollDistance = 0;
     }
+
     prevScrollPos = currentScrollPos;
   }
 
