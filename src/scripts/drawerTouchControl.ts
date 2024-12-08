53c66db67d9482f6ccb0194fe7efd381f58bd491
astio <github@mail.astn.io>
2024-12-08 15:05:14 -0800

    Refactored code, removed unused code and comments

--- src/scripts/drawerTouchControl.ts
+++ src/scripts/drawerTouchControl.ts
@@ -1,52 +1,10 @@
 export function initDrawerTouch() {
   document.addEventListener('astro:page-load', () => {
-    /*** TESTING  ***/
-
-    // document.addEventListener('touchstart', (event) => {
-    //   [...event.changedTouches].forEach((touch) => {
-    //     const dot = document.createElement('div');
-
-    //     dot.classList.add('touch-dot');
-    //     dot.style.top = `${touch.pageY}px`;
-    //     dot.style.left = `${touch.pageX}px`;
-    //     dot.id = touch.identifier.toString();
-
-    //     document.body.append(dot);
-    //   });
-    // });
-
-    // document.addEventListener('touchmove', (event) => {
-    //   [...event.changedTouches].forEach((touch) => {
-    //     const dot = document.getElementById(touch.identifier.toString());
-
-    //     dot!.style.top = `${touch.pageY}px`;
-    //     dot!.style.left = `${touch.pageX}px`;
-    //   });
-    // });
-
-    // document.addEventListener('touchend', (event) => {
-    //   [...event.changedTouches].forEach((touch) => {
-    //     const dot = document.getElementById(touch.identifier.toString());
-
-    //     dot!.remove();
-    //   });
-    // });
-
-    /*** Actual Logic  ***/
     const navDrawer = document.getElementById('nav-drawer') as HTMLElement;
     const navDrawerToggle = document.getElementById('btn-open-nav-drawer');
 
     const navDrawerTransitionDuration = '0.35s';
-
-    const touchDirection = {
-      left: -1,
-      right: 1,
-    };
-
-    let startTouchDirection = {
-      initiated: false,
-      direction: 0,
-    };
+    const openThreshold: number = -75;
 
     let deltaY = 0;
     let deltaX = 0;
@@ -60,7 +18,6 @@ export function initDrawerTouch() {
     let totalTouchDistance: number = 0;
     let normalizedTouchDistance: number = 0;
     let currTouchDirection: number = 0;
-    const openThreshold: number = -75;
 
     function normalizeRange(val: number, max: number, min: number) {
       return Math.round(((val - min) / (max - min)) * 100);
@@ -73,7 +30,6 @@ export function initDrawerTouch() {
     }
 
     function closeDrawer() {
-      console.log('closing lol');
       navDrawer!.setAttribute('data-active', 'false');
       navDrawer!.style.transform = 'translateX(-100%)';
       navDrawerToggle!.setAttribute('data-action', 'open');
@@ -83,30 +39,13 @@ export function initDrawerTouch() {
       totalTouchDistance = 0;
       normalizedTouchDistance = 0;
       currTouchDirection = 0;
-      startTouchDirection.initiated = false;
-    }
-
-    function touchMoveLeft(): boolean {
-      return currTouchDirection <= touchDirection.left;
-    }
-
-    function touchMoveRight(): boolean {
-      return currTouchDirection >= touchDirection.right;
-    }
-
-    function startTouchLeft(): boolean {
-      return startTouchDirection.direction <= touchDirection.left;
-    }
-
-    function startTouchRight(): boolean {
-      return startTouchDirection.direction >= touchDirection.right;
     }
 
     function drawerTransform(normalizedDistance: number): number {
       return -100 + normalizedDistance;
     }
 
-    function moveDrawer() {
+    function touchOpenDrawer() {
       normalizedTouchDistance = normalizeRange(
         Math.abs(totalTouchDistance),
         window.innerWidth,
@@ -115,9 +54,7 @@ export function initDrawerTouch() {
 
       normalizedTouchDistance = drawerTransform(normalizedTouchDistance);
 
-      if (touchDirection.right) {
-        navDrawer!.style.transform = `translateX(${normalizedTouchDistance}%)`;
-      }
+      navDrawer!.style.transform = `translateX(${normalizedTouchDistance}%)`;
     }
 
     document.addEventListener(
@@ -141,32 +78,24 @@ export function initDrawerTouch() {
       (event) => {
         const touch = event.changedTouches[0];
 
+        totalTouchDistance = touch.clientX - startTouchX;
+        currTouchDirection = touch.clientX - prevTouchX;
+
         if (!currentlyTouching) {
           deltaY = Math.abs(touch.clientY - startTouchY);
           deltaX = Math.abs(touch.clientX - startTouchX);
         }
 
-        totalTouchDistance = touch.clientX - startTouchX;
-        currTouchDirection = touch.clientX - prevTouchX;
-
-        if (!startTouchDirection.initiated) {
-          startTouchDirection.direction = currTouchDirection;
-        }
-
-        startTouchDirection.initiated = true;
-
-        if (deltaX > deltaY && event.cancelable) {
-          // No page scrolling, can open drawer
+        const userTouchOpenDrawer = deltaX > deltaY && event.cancelable;
+        if (userTouchOpenDrawer) {
           event.preventDefault();
-          moveDrawer();
+          touchOpenDrawer();
         } else {
-          // Cannot open drawer, allowed page scrolling
           return;
         }
+        currentlyTouching = true;
 
         prevTouchX = touch.clientX;
-
-        currentlyTouching = true;
       },
       { passive: false }
     );
