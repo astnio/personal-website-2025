444d80b9841d3559b86cc4b0384a550cab95b340
astio <github@mail.astn.io>
2024-12-10 11:32:59 -0800

    Contact dialog now has transitions. User can also click on the backdrop to close it

--- src/scripts/openContactDialog.ts
+++ src/scripts/openContactDialog.ts
@@ -3,10 +3,60 @@ document.addEventListener('astro:page-load', () => {
   const contactModal = document.getElementById(
     'contact-dialog'
   ) as HTMLDialogElement;
+  const btnCloseModal = document.getElementById('btn-contact-dialog-close');
+  const contactForm = document.getElementById('contact-form');
+
+  const animationBufferMs = -5;
+  const animationDurationMs = 500 + animationBufferMs;
+
+  function setFadeIn() {
+    contactModal.classList.remove('dialog-fade-out', 'backdrop-fade-out');
+    contactModal.classList.add('dialog-fade-in', 'backdrop-fade-in');
+  }
+
+  function setFadeOut() {
+    contactModal.classList.remove('dialog-fade-in', 'backdrop-fade-in');
+    contactModal.classList.add('dialog-fade-out', 'backdrop-fade-out');
+  }
+
+  function openModal() {
+    setFadeIn();
+    contactModal.showModal();
+  }
+
+  function closeModal() {
+    setFadeOut();
+    contactModal.close();
+  }
+
+  function timeoutClose() {
+    setFadeOut();
+    setTimeout(() => {
+      contactModal.close();
+    }, animationDurationMs);
+  }
+
+  contactModal.addEventListener('close', setFadeOut);
+
+  contactModal.addEventListener('click', (event) => {
+    const modalDimensions = contactModal.getBoundingClientRect();
+
+    if (
+      event.clientX < modalDimensions.left ||
+      event.clientX > modalDimensions.right ||
+      event.clientY < modalDimensions.top ||
+      event.clientY > modalDimensions.bottom
+    ) {
+      timeoutClose();
+    }
+  });
 
   contactButtons.forEach((btnContact) => {
-    btnContact.addEventListener('click', () => {
-      contactModal.showModal();
-    });
+    btnContact.addEventListener('click', openModal);
+  });
+
+  contactForm!.addEventListener('submit', (event) => {
+    event.preventDefault();
+    timeoutClose();
   });
 });
