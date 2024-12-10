43385dfcd32478442ec5a47f4616cca9e871b436
astio <github@mail.astn.io>
2024-12-09 23:24:48 -0800

    Added hidden overlay so user can just click out of the customization menu to close it

--- src/scripts/customizationMenuButton.ts
+++ src/scripts/customizationMenuButton.ts
@@ -3,15 +3,35 @@ document.addEventListener('astro:page-load', () => {
     '.btn-show-customization-options'
   );
 
+  const customizationOverlays = document.querySelectorAll(
+    '.customization-menu-overlay'
+  );
+
   function handleOnClick(event: Event) {
     const btn = event.target as HTMLElement;
     const menuWrapper = btn.closest('.customization-menu-wrapper');
     const menu = menuWrapper!.querySelector('.customization-menu');
 
     menu?.setAttribute('data-active', 'true');
+
+    customizationOverlays.forEach((overlay) => {
+      overlay.setAttribute('data-active', 'true');
+    });
   }
 
   customizationButtons.forEach((button) => {
     button.addEventListener('click', handleOnClick);
+  });
+
+  customizationOverlays.forEach((overlay) => {
+    overlay.addEventListener('click', () => {
+      customizationButtons.forEach((button) => {
+        const menuWrapper = button.closest('.customization-menu-wrapper');
+        const menu = menuWrapper!.querySelector('.customization-menu');
+
+        menu?.setAttribute('data-active', 'false');
+      });
+      overlay.setAttribute('data-active', 'false');
+    });
   });
 });
