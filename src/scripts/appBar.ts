92ad4f2a5810ed39a3de5e1427b2736c8566e9fc
astio <github@mail.astn.io>
2024-12-20 23:50:58 -0800

    Blog navbar now moves with appbar

--- src/scripts/appBar.ts
+++ src/scripts/appBar.ts
@@ -1,8 +1,14 @@
 document.addEventListener('astro:page-load', () => {
   const navbar = document.getElementById('app-bar');
   const drawerToggle = document.getElementById('btn-open-nav-drawer');
+  const blogNavWrapper = document.querySelector(
+    '.blog-nav-wrapper'
+  ) as HTMLElement;
 
   const scrollActivationDistance = 150;
+
+  const blogNavWrapperTopRemAppBarOn = 4;
+  const blogNavWrapperTopRemAppBarOff = 0;
 
   let totalScrollDistance = 0;
   let prevScrollPos = window.scrollY;
@@ -12,11 +18,19 @@ document.addEventListener('astro:page-load', () => {
   function hideAppBar() {
     navbar!.style.transform = 'translateY(-100%)';
     drawerToggle!.style.transform = 'translateY(-100%)';
+
+    if (blogNavWrapper) {
+      blogNavWrapper!.style.top = `${blogNavWrapperTopRemAppBarOff}rem`;
+    }
   }
 
   function revealAppBar() {
     navbar!.style.transform = 'translateY(0%)';
     drawerToggle!.style.transform = 'translateY(0%)';
+
+    if (blogNavWrapper) {
+      blogNavWrapper!.style.top = `${blogNavWrapperTopRemAppBarOn}rem`;
+    }
   }
 
   function updateScrollDireciton(scrollDir: number) {
