47cfc349d245e8aff20dcc13eb486b301221fa5f
astnio <github@mail.astn.io>
2025-02-25 16:02:58 -0800

    Updated and added icons

--- src/scripts/utils/iconHelper.ts
+++ src/scripts/utils/iconHelper.ts
@@ -1,18 +1,22 @@
 const icons = {
-    JavaScript: 'bx bxl-javascript',
-    React: 'bx bxl-react',
-    TypeScript: 'bx bxl-typescript',
-    Node: 'bx bxl-nodejs',
-    CSS: 'bx bxl-css3',
-    HTML: 'bx bxl-html5',
-    Python: 'bx bxl-python',
-    Git: 'bx bxl-git',
-    MongoDB: 'bx bxl-mongodb',
-    PostgreSQL: 'bx bxl-postgresql',
-    TailwindCSS: 'bx bxl-tailwind-css'
-}
+  JavaScript: 'bx bxl-javascript',
+  React: 'bx bxl-react',
+  TypeScript: 'bx bxl-typescript',
+  Node: 'bx bxl-nodejs',
+  CSS: 'bx bxl-css3',
+  HTML: 'bx bxl-html5',
+  Python: 'bx bxl-python',
+  Git: 'bx bxl-git',
+  MongoDB: 'bx bxl-mongodb',
+  PostgreSQL: 'bx bxl-postgresql',
+  Tailwind: 'bx bxl-tailwind-css',
+  Skeleton: 'bx bxs-skull',
+  Svelte: 'bx bx-code-block',
+  SvelteKit: 'bx bx-code-block',
+  Vite: 'bx bx-code-block',
+};
 
 export function getIcon(name: string) {
-    const icon = icons[name as keyof typeof icons];
-    return icon ?? 'bx bx-code-alt';
+  const icon = icons[name as keyof typeof icons];
+  return icon ?? 'bx bx-code-alt';
 }
