27a8d09aeb417b5a3776645019a2f3b8cc525911
astnio <github@mail.astn.io>
2025-03-20 15:39:19 -0700

    Images now seamlessly transition (except for when doing a full loop)

--- src/scripts/projectGalleryNavigation.ts
+++ src/scripts/projectGalleryNavigation.ts
@@ -5,40 +5,63 @@ document.addEventListener('astro:page-load', () => {
     document.querySelector('.images-wrapper')!;
 
   const projectImages = projectImagesElement.children;
+  const transitionDuration = '0.69s';
+  const translateDistance = 102;
 
   let currentIndex = 0;
 
-  function init() {
-    projectImages[0]!.setAttribute('data-active', 'true');
-  }
+  function positionImagesAsStrip() {
+    for (let i = 0; i < projectImages.length; i++) {
+      const projectImage = projectImages[i] as HTMLElement;
 
-  function currentImageToFirst() {
-    if (currentIndex >= projectImagesElement.children.length) {
-      currentIndex = 0;
+      const position = (i - currentIndex) * translateDistance;
+      projectImage.style.transform = `translateX(${position}%)`;
+
+      projectImage.setAttribute(
+        'data-active',
+        i === currentIndex ? 'true' : 'false'
+      );
     }
   }
 
-  function currentImageToLast() {
-    if (currentIndex < 0) {
-      currentIndex = projectImagesElement.children.length - 1;
+  function init() {
+    disableTransitions();
+    positionImagesAsStrip();
+    enableTransitions();
+  }
+
+  function disableTransitions() {
+    for (let i = 0; i < projectImages.length; i++) {
+      const projectImage = projectImages[i] as HTMLElement;
+      projectImage.style.transitionDuration = '0s';
+    }
+  }
+
+  function enableTransitions() {
+    for (let i = 0; i < projectImages.length; i++) {
+      const projectImage = projectImages[i] as HTMLElement;
+      projectImage.style.transitionDuration = transitionDuration;
     }
   }
 
   function navImage(isNextImage: boolean) {
-    projectImages[currentIndex].setAttribute('data-active', 'false');
-
     if (isNextImage) {
       currentIndex++;
-      currentImageToFirst();
+      if (currentIndex >= projectImages.length) {
+        currentIndex = 0;
+      }
     } else {
       currentIndex--;
-      currentImageToLast();
+      if (currentIndex < 0) {
+        currentIndex = projectImages.length - 1;
+      }
     }
 
-    projectImages[currentIndex].setAttribute('data-active', 'true');
+    positionImagesAsStrip();
   }
 
   btnNext!.addEventListener('click', () => navImage(true));
   btnPrev!.addEventListener('click', () => navImage(false));
+
   init();
 });
